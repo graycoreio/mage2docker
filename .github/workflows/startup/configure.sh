ba7ca69ea2fca40a2b79786db7db97ec0b759c03
Damien Retzinger <damienwebdev@gmail.com>
2022-08-04 14:10:52 -0400

    test: add a CI pipeline to verify that installation works (#64)
    Co-authored-by: Mateus Dutra Dadalto <mateusdadalto@gmail.com>

---
+++ .github/workflows/startup/configure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+MAGENTO_VERSION_2_3='magento/project-community-edition=2.3.*'
+MAGENTO_VERSION_2_4='magento/project-community-edition=2.4.*'
+
+case "$COMPOSER_PROJECT" in
+   "$MAGENTO_VERSION_2_3")
+        printf "\\n\\nCOMPOSE_FILE=docker-compose.yml:./compose/base/2.3.yml:compose/sync/containersync.yml:compose/php/7.4/build.yml" >> .env
+        sed -i -e "s|^[#]*\s*COMPOSER_PROJECT=.*|COMPOSER_PROJECT="$COMPOSER_PROJECT"|" .env
+      ;;
+   $MAGENTO_VERSION_2_4)
+        printf "\\n\\nCOMPOSE_FILE=docker-compose.yml:./compose/base/2.4.yml:compose/sync/containersync.yml:compose/php/7.4/build.yml" >> .env
+        sed -i -e "s|^[#]*\s*COMPOSER_PROJECT=.*|COMPOSER_PROJECT="$COMPOSER_PROJECT"|" .env
+      ;;
+   *)
+        printf "Unknown Magento version"
+        exit 1;
+     ;;
+esac
