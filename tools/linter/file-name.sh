9633d13665d61cd7bb2deb266d18ced2122cdc38
Damien Retzinger <damienwebdev@gmail.com>
2022-08-07 09:29:26 -0400

    i cant write bash

--- tools/linter/file-name.sh
+++ tools/linter/file-name.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e;
 
-echo "Linting file names, only allowing .yml";
-
-paths=`git ls-files | grep .yaml`;
-
+paths=`git ls-files | grep .yaml || true`;
 count=`echo $paths | wc -w`;
 
-if [ $count != 0 ]
+if [ "$count" != "0" ]; then
     echo "$count file(s) discovered";
     for item in $paths; do 
         echo $item;
     done;
     exit 1;
-then 
+else 
     exit 0;
 fi 
